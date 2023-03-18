9146d8976930b8601bc184184768d87041dd76df
Bazirakye Tonny Mubangizi <bazirakyetonny15@gmail.com>
2023-03-18 02:46:59 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    i = 0;
+    while (i < 10)
+    {
+        putchar(i + '0');
+        i++;
+    }
+
+    putchar('\n');
+    return (0);
+}
