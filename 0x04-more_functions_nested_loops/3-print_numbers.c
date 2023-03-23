307ea68d33bc66afd05b7a6aa7c236157afbf565
Bazirakye Tonny Mubangizi <bazirakyetonny15@gmail.com>
2023-03-23 10:30:32 -0700

    print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints numbers 0 to 9
+ *
+ * Return: returns nothing
+ *
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (; i < 9; i++)
+	{
+		return (i);
+	}
+}
