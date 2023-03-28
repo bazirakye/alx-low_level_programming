79f48f5c3fe646e572dafb1877c563510c34d207
Bazirakye Tonny Mubangizi <bazirakyetonny15@gmail.com>
2023-03-27 18:33:13 -0700

    change value of pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Fuction to change value of 98
+ *
+ * @n: paramenter to pass in the value
+ *
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
