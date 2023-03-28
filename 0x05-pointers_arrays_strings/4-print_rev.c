89f5138f3efcb1916115cf0fac46ad6ce7a5065c
Bazirakye Tonny Mubangizi <bazirakyetonny15@gmail.com>
2023-03-27 19:23:27 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse to stdout, followed by a new line
+ * @s: The string to be printed in reverse
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (int i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
