c7482ed857fad79aa9fc586f78d5e365460c6a18
Bazirakye Tonny Mubangizi <bazirakyetonny15@gmail.com>
2023-04-03 12:22:24 -0700

    fill memory with a single character

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.c"
+
+/**
+ * _memset - fills memory with constant byte
+ * @s: pointer to memory area that is to be filled
+ * @b: value to be set
+ * @n: number of bytes to be set
+ * Return: returns pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+
+		return (s);
+	}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointers to pointers and multi-dimensional arrays
