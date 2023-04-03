369c33490a94633d3236a8542af17762f056f9c5
Bazirakye Tonny Mubangizi <bazirakyetonny15@gmail.com>
2023-04-03 13:13:06 -0700

    cpy character to all address

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int = 0;
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
 		s[i] = b;
-
-		return (s);
+		i++;
 	}
+	return (s);
