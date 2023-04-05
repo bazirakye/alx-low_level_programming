18aa80662639d7178f93d2e523e394c4775e962c
Bazirakye Tonny Mubangizi <bazirakyetonny15@gmail.com>
2023-04-05 02:58:01 -0700

    change set value of a character using derefrensing a pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - sets value of pointer to a character
+ * @s: pointer to pointer to char
+ * @to: pointer to character
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
