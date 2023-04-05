9a056d157d9c575d5ca3a78d9d5ba8a42d85374b
Bazirakye Tonny Mubangizi <bazirakyetonny15@gmail.com>
2023-04-05 13:23:08 -0700

    recursion on string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,6 +5,7 @@
  * _puts_recursion - recursive function to print the string
  * put_char - whrite character
  * @s: pointer to the character
+ * @c: argument to the put_char function
  * return: returns at base case
  */
 void put_char(char c)
@@ -18,4 +19,10 @@ void _puts_recursion(char *s)
 		return;
 	put_char(*s);
 	_puts_recursion(s + 1);
+}
+int main void()
+{
+	_puts_recursion();
+	put_char('\n');
+	return (0);
 }
