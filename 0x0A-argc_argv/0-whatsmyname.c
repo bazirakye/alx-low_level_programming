272c171cfca1393ce68c9463c0f81d0debd5cbd7
Bazirakye Tonny Mubangizi <bazirakyetonny15@gmail.com>
2023-04-10 22:45:57 -0700

    program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Main function
+ * @argc: argument counter
+ * @argv: argument vector array
+ * Return: return 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
