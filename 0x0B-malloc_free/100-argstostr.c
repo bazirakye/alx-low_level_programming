e856a7223a9a4dc2c084cc8cd3a35fc4c50a22f2
Bazirakye Tonny Mubangizi <bazirakyetonny15@gmail.com>
2023-04-12 12:43:51 -0700

    concatinate arguments to form a string

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatinates arguments to form a string
+ * @ac: argument counter
+ * @av: argument vector string array
+ * Return: pointer to the concatinated string or null if no space
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j, k = 0, len = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+		len++;
+	}
+	len++;
+
+	str = malloc(sizeof(char) * len);
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+
+	return (str);
+}
