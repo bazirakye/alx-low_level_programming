c92c080bbd084ea8cc823ea8820a17aab3402fa3
Bazirakye Tonny Mubangizi <bazirakyetonny15@gmail.com>
2023-04-17 06:58:50 -0700

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * dog - newly created data stucture
+ * @name: name member for dog
+ * @age: age member for dog
+ * @owner: owner member for dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 #endif
