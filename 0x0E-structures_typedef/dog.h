be1ee7d1c9bdbdaacbd145dc8dd737d5dfca0260
Bazirakye Tonny Mubangizi <bazirakyetonny15@gmail.com>
2023-04-17 07:04:45 -0700

    dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,11 @@
 #define DOG_H
 
 /**
- * dog - newly created data stucture
+ * struct dog - newly created data stucture
  * @name: name member for dog
  * @age: age member for dog
  * @owner: owner member for dog
+ *
  * Description: This structure gives details about a dog
  */
 struct dog
