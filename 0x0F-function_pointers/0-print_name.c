34f010888b8292ca6680031e06b44e84e03c18ed
Bazirakye Tonny Mubangizi <bazirakyetonny15@gmail.com>
2023-04-19 11:29:56 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,10 +3,11 @@
 /**
  * print_name -  prints the name function
  * @name: character array for name to be printed
- * @f: function pointer that takes in character array as argument and points to functionthat prints a name
+ * @f: function pointer that takes in character
+ * array as argument and points to functionthat prints a name
  */
 
-void print_name(char *name, (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
 	{
