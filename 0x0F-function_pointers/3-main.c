a856bcdb9feaeeffaba5e86bbb685f39602b8e2a
Bazirakye Tonny Mubangizi <bazirakyetonny15@gmail.com>
2023-04-20 06:58:59 -0700

    error resolved

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,24 +1,27 @@
 #include "3-calc.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 /**
- * main - entry point
- * @argc: argument counter
- * @argv: argument vector
- * Return:0
- */
+* main - entry point
+* @argc: # of args
+* @argv: array of pointers to names of args
+* Return: 0
+*/
 int main(int argc, char **argv)
 {
+	/* input checking */
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
-		{
-			printf("Error\n");
-			exit(100);
-		}
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	/* calling get_op_func with arguments */
 	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
