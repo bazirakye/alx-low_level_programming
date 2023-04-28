25fd6180358373341f1907f90eafd4fddd0a4e43
bazirakye <bazirakyetonny15@gmail.com>
2023-04-28 10:32:46 -0700

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * before_main - function hat prints befor main function excecutes
+ */
+
+void __attribute__ ((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
