bb518738f7aa85d2758c1505461f1427f14c9c96
bazirakye <bazirakyetonny15@gmail.com>
2023-05-02 11:39:25 -0700

    adding data in list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function that adds linke list data
+ * @head: first node
+ * Return: sum of data in linked list;
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	listint_t *node;
+
+	node = head;
+
+	if (node == NULL)
+		return (0);
+
+	while (node->next != NULL)
+	{
+		node = node->next;
+
+		sum += node->n;
+	}
+
+	return (sum);
+}
