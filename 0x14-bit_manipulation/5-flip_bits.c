430f31fbd90f5d843d52a64ff64a595dbd8fee90
bazirakye <bazirakyetonny15@gmail.com>
2023-05-04 13:37:20 -0700

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * flip_bits - flip bits
+ * @n: number one
+ * @m: number 2
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int bits;
+
+	for (bits = 0; n || m; n >>=1, n >>=1)
+	{
+		if ((n & 1) != (m & 1))
+			bits++;
+	}
+	return (bits);
+}
